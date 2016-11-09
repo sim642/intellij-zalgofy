c9e37c8326b7ded1d7c221179f80be496c634867
Simmo Saan <simmo.saan@gmail.com>
2016-11-09 10:09:25 +0200

    Reformat fix to indent size 4

--- src/eu/sim642/idea/zalgofy/ZalgofyFix.java
+++ src/eu/sim642/idea/zalgofy/ZalgofyFix.java
@@ -13,40 +13,40 @@ import java.util.function.UnaryOperator;
 
 public class ZalgofyFix extends InspectionGadgetsFix {
 
-  private final UnaryOperator<String> operator = new ZalgoIdentifierOperator();
+    private final UnaryOperator<String> operator = new ZalgoIdentifierOperator();
 
-  @NotNull
-  @Override
-  public String getFamilyName() {
-    return "Zalgofy";
-  }
+    @NotNull
+    @Override
+    public String getFamilyName() {
+        return "Zalgofy";
+    }
 
-  @Override
-  @NotNull
-  public String getName() {
-    return "Zalgofy";
-  }
+    @Override
+    @NotNull
+    public String getName() {
+        return "Zalgofy";
+    }
 
-  @Override
-  public void doFix(final Project project, final ProblemDescriptor descriptor) {
-    final PsiElement nameIdentifier = descriptor.getPsiElement();
-    final PsiElement elementToRename = nameIdentifier.getParent();
+    @Override
+    public void doFix(final Project project, final ProblemDescriptor descriptor) {
+        final PsiElement nameIdentifier = descriptor.getPsiElement();
+        final PsiElement elementToRename = nameIdentifier.getParent();
 
-    final RefactoringFactory factory = RefactoringFactory.getInstance(project);
-    final RenameRefactoring renameRefactoring =
-      factory.createRename(elementToRename, operator.apply(((PsiNamedElement) elementToRename).getName()), false, false);
-    renameRefactoring.setInteractive(null);
-    renameRefactoring.setPreviewUsages(false);
-    renameRefactoring.run();
-  }
+        final RefactoringFactory factory = RefactoringFactory.getInstance(project);
+        final RenameRefactoring renameRefactoring =
+                factory.createRename(elementToRename, operator.apply(((PsiNamedElement) elementToRename).getName()), false, false);
+        renameRefactoring.setInteractive(null);
+        renameRefactoring.setPreviewUsages(false);
+        renameRefactoring.run();
+    }
 
-  @Override
-  public boolean startInWriteAction() {
-    return false;
-  }
+    @Override
+    public boolean startInWriteAction() {
+        return false;
+    }
 
-  @Override
-  protected boolean prepareForWriting() {
-    return false;
-  }
+    @Override
+    protected boolean prepareForWriting() {
+        return false;
+    }
 }
