6fe9e2ca2a93f5e8e58b25ad4bcc0479cf65fc38
Simmo Saan <simmo.saan@gmail.com>
2016-11-16 11:42:17 +0200

    Allow comments to be zalgofied (#1)

--- src/eu/sim642/idea/zalgofy/ZalgofyAction.java
+++ src/eu/sim642/idea/zalgofy/ZalgofyAction.java
@@ -1,11 +1,15 @@
 package eu.sim642.idea.zalgofy;
 
 import com.intellij.openapi.actionSystem.DataContext;
+import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
+import com.intellij.psi.JavaTokenType;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiNamedElement;
+import com.intellij.psi.impl.source.tree.PsiCommentImpl;
+import com.intellij.psi.tree.IElementType;
 import com.intellij.refactoring.RefactoringActionHandler;
 import com.intellij.refactoring.RefactoringFactory;
 import com.intellij.refactoring.actions.BaseRefactoringAction;
@@ -34,16 +38,43 @@ public class ZalgofyAction extends BaseRefactoringAction {
         return new RefactoringActionHandler() {
             @Override
             public void invoke(@NotNull Project project, Editor editor, PsiFile file, DataContext dataContext) {
-                invoke(project, BaseRefactoringAction.getPsiElementArray(dataContext), dataContext);
+                invoke(project, new PsiElement[]{BaseRefactoringAction.getElementAtCaret(editor, file)}, dataContext);
             }
 
             @Override
             public void invoke(@NotNull Project project, @NotNull PsiElement[] elements, DataContext dataContext) {
                 RefactoringFactory refactoringFactory = RefactoringFactory.getInstance(project);
                 for (PsiElement element : elements) {
-                    String name = ((PsiNamedElement) element).getName();
-                    String newName = operator.apply(name);
-                    refactoringFactory.createRename(element, newName).run();
+                    if (element instanceof PsiNamedElement) {
+                        String name = ((PsiNamedElement) element).getName();
+                        String newName = operator.apply(name);
+                        refactoringFactory.createRename(element, newName).run();
+                    }
+                    else if (element instanceof PsiCommentImpl) {
+                        IElementType tokenType = ((PsiCommentImpl) element).getTokenType();
+
+                        String text = element.getText();
+                        String innerText;
+                        if (tokenType.equals(JavaTokenType.C_STYLE_COMMENT))
+                            innerText = text.substring(2, text.length() - 4);
+                        else if (tokenType.equals(JavaTokenType.END_OF_LINE_COMMENT))
+                            innerText = text.substring(2);
+                        else
+                            continue; // unknown comment type
+
+                        String newInnerText = operator.apply(innerText);
+                        String newText;
+                        if (tokenType.equals(JavaTokenType.C_STYLE_COMMENT))
+                            newText = "/*" + newInnerText + "*/";
+                        else if (tokenType.equals(JavaTokenType.END_OF_LINE_COMMENT))
+                            newText = "//" + newInnerText;
+                        else
+                            continue; // unknown comment type
+
+                        WriteCommandAction.runWriteCommandAction(project, () -> {
+                            ((PsiCommentImpl) element).updateText(newText);
+                        });
+                    }
                 }
             }
         };
